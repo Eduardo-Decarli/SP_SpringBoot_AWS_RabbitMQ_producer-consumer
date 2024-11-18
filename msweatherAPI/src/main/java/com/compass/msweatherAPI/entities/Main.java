7bd8a1c167270e1f0c7bccd3873388532c2e1473
Eduardo Decarli <decarliedu@gmail.com>
2024-11-18 11:13:57 -0300

    :sparkles: feat: create entities of weather response

---
+++ msweatherAPI/src/main/java/com/compass/msweatherAPI/entities/Main.java
@@ -0,0 +1,11 @@
+package com.compass.msweatherAPI.entities;
+
+public class Main {
+
+    private double temp;
+    private double temp_min;
+    private double temp_max;
+    private double pressure;
+    private double humidity;
+
+}
---
+++ msweatherAPI/src/main/java/com/compass/msweatherAPI/entities/Weather.java
@@ -0,0 +1,7 @@
+package com.compass.msweatherAPI.entities;
+
+public class Weather {
+
+    private String main;
+    private String description;
+}
